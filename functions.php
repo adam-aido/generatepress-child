37b9a52566d2612d1b4073dead02478953c5dea3
Adam Antoszczak <adam@aido.pl>
2023-05-15 12:56:20 +0200

    check if constants are defined

--- functions.php
+++ functions.php
@@ -56,15 +56,17 @@ function adamant_loginlogo_url($url)
  */
 add_action( 'phpmailer_init', 'adamant_mailer' );
 function adamant_mailer( $phpmailer ) {
-    $phpmailer->isSMTP();
-    $phpmailer->Host = SMTP_HOST;
-    $phpmailer->SMTPAuth = SMTP_AUTH;
-    $phpmailer->Port = SMTP_PORT;
-    $phpmailer->Username = SMTP_USER;
-    $phpmailer->Password = SMTP_PASS;
-    $phpmailer->SMTPSecure = SMTP_SECURE;
-    $phpmailer->From = SMTP_FROM;
-    $phpmailer->FromName = SMTP_NAME;
+	if(isset(SMTP_HOST)) {
+		$phpmailer->isSMTP();
+		$phpmailer->Host = SMTP_HOST;
+		$phpmailer->SMTPAuth = SMTP_AUTH;
+		$phpmailer->Port = SMTP_PORT;
+		$phpmailer->Username = SMTP_USER;
+		$phpmailer->Password = SMTP_PASS;
+		$phpmailer->SMTPSecure = SMTP_SECURE;
+		$phpmailer->From = SMTP_FROM;
+		$phpmailer->FromName = SMTP_NAME;
+	}
 }
 
 /**
